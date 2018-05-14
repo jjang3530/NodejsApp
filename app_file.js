8aa3f0b68fa5ce3fef6ef444408e764a91b76e02
jayjang <jjang3530@conestogac.on.ca>
2018-05-14 11:10:48 -0400

    basic Code refactoring

--- app_file.js
+++ app_file.js
@@ -9,35 +9,54 @@ app.set('views', './views_file');
 app.set('view engine', 'jade');
 
 app.get('/topic/new', function(req, res){
-  res.render('new');
-})
-
-app.get('/topic', function(req, res){
   fs.readdir('data', function(err, files){
     if (err) {
       console.log(err);
       res.status(500).send('Internal Server Error');
     }
-    res.render('view', {topics:files});
+      res.render('new', {topics:files});
+  });
+})
+
+app.get(['/topic', '/topic/:id'] , function(req, res){
+  fs.readdir('data', function(err, files){
+    if (err) {
+      console.log(err);
+      res.status(500).send('Internal Server Error');
+    }
+    var id = req.params.id;
+    if (id) {
+      fs.readFile('data/'+id, 'utf8', function(err, data){
+        if (err) {
+          console.log(err);
+          res.status(500).send('Internal server error');
+        }
+        res.render('view', {topics:files, title:id, description:data});
+      })
+
+    }else {
+    res.render('view', {topics:files, title:'Welcome', description:'JavaScript for server'});
+    }
+
   })
 });
 
-app.get('/topic/:id', function(req, res){
-  var id = req.params.id;
-  fs.readdir('data', function(err, files){
-    if (err) {
-      console.log(err);
-      res.status(500).send('Internal Server Error');
-    }
-    fs.readFile('data/'+id, 'utf8', function(err, data){
-      if (err) {
-        console.log(err);
-        res.status(500).send('Internal server error');
-      }
-      res.render('view', {topics:files, title:id, description:data});
-    })
-  })
-})
+// app.get('/topic/:id', function(req, res){
+//   var id = req.params.id;
+//   fs.readdir('data', function(err, files){
+//     if (err) {
+//       console.log(err);
+//       res.status(500).send('Internal Server Error');
+//     }
+//     fs.readFile('data/'+id, 'utf8', function(err, data){
+//       if (err) {
+//         console.log(err);
+//         res.status(500).send('Internal server error');
+//       }
+//       res.render('view', {topics:files, title:id, description:data});
+//     })
+//   })
+// })
 
 app.post('/topic', function(req, res){
   var title = req.body.title;
@@ -47,7 +66,7 @@ app.post('/topic', function(req, res){
         console.log(err);
         res.status(500).send('Internal Server Error');
     }
-  res.send('Success!')
+  res.redirect('/topic/'+ title);
   });
 })
 
