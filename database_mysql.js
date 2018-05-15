64404c8ca236312415514b3461a0f961aff38485
jayjang <jjang3530@conestogac.on.ca>
2018-05-15 09:07:16 -0400

    node_mysql Update & delete

--- database_mysql.js
+++ database_mysql.js
@@ -21,16 +21,35 @@ connection.connect();
 // connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
 //   if (error) throw error;
 //   console.log('The solution is: ', results[0].solution);
-// });
 //
 
-var sql = 'INSERT INTO topic (title, description, author) VALUES(?, ?, ?)';
-var params = ['Supervisor', 'Watcher', 'graphy'];
+// var sql = 'INSERT INTO topic (title, description, author) VALUES(?, ?, ?)';
+// var params = ['Supervisor', 'Watcher', 'graphy'];
+// connection.query(sql, params, function(err, rows, fields){
+//   if (err) {
+//     console.log(err);
+//   }else {
+//     console.log(rows.insertId);
+//   }
+// })
+
+// var sql = 'UPDATE topic SET title=?, description=? WHERE id=?';
+// var params = ['NPM', 'jinah', 1];
+// connection.query(sql, params, function(err, rows, fields){
+//   if (err) {
+//     console.log(err);
+//   }else {
+//     console.log(rows);
+//   }
+// })
+
+var sql = 'DELETE FROM topic WHERE id=?';
+var params = [1];
 connection.query(sql, params, function(err, rows, fields){
   if (err) {
     console.log(err);
   }else {
-    console.log(rows.insertId);
+    console.log(rows);
   }
 })
 
